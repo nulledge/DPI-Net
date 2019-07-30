aaedfb83e2beddabc1e2d0987d39933ee3c57be9
nulledge <the.nulledge@gmail.com>
2019-07-30 16:50:21 +0900

    Resume training on LiquidFun data

--- test_liquidfun.py
+++ test_liquidfun.py
@@ -1,6 +1,8 @@
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-env', '--environment', default='LiquidFun')
+parser.add_argument('-e', '--epoch', type=int, default=-1)
+parser.add_argument('-iter', '--iteration', type=int, default=0)
 args = parser.parse_args()
 
 if __name__ == '__main__':
@@ -26,6 +28,11 @@ if __name__ == '__main__':
     from models import DPINet
     model = DPINet(config['train'], dataset['train'].stats, None, residual=True, use_gpu=torch.cuda.is_available())
 
+    if args.epoch >= 0 or args.iteration >= 0:
+        ckpt_path = '%s/ckpt/net_epoch_%d_iter_%d.pth' % (config['train'].outf, args.epoch, args.iteration)
+        print('load model:', ckpt_path)
+        model.load_state_dict(torch.load(ckpt_path))
+
     if config['train'].verbose_model:
         from utils import count_parameters
         print("Number of parameters: %d" % count_parameters(model))
@@ -43,7 +50,7 @@ if __name__ == '__main__':
     from tqdm import tqdm
     from torch.autograd import Variable
     best_valid_loss = np.inf
-    for epoch in range(config['train'].n_epoch):
+    for epoch in range(args.epoch + 1, config['train'].n_epoch):
 
         from torch.utils.tensorboard import SummaryWriter
         from datetime import datetime
@@ -62,8 +69,8 @@ if __name__ == '__main__':
 
             with torch.set_grad_enabled(phase == 'train'):
                 losses = 0
-                with tqdm(loader[phase]) as progress:
-                    for data in progress:
+                with tqdm(total=len(loader[phase])) as progress:
+                    for data in loader[phase]:
                         attr, state, rels, n_particles, n_shapes, instance_idx, label = data
                         Ra, node_r_idx, node_s_idx, pstep = rels[3], rels[4], rels[5], rels[6]
 
@@ -94,15 +101,10 @@ if __name__ == '__main__':
 
                         attr, state, Rr, Rs, Ra, label = data
 
-                        # st_time = time.time()
                         predicted = model(
                             attr, state, Rr, Rs, Ra, n_particles,
                             node_r_idx, node_s_idx, pstep,
                             instance_idx, config[phase].material, config[phase].verbose_model)
-                        # print('Time forward', time.time() - st_time)
-
-                        # print(predicted)
-                        # print(label)
 
                         loss = criterionMSE(predicted, label)
                         losses += np.sqrt(loss.item())
@@ -110,7 +112,7 @@ if __name__ == '__main__':
                         if phase == 'train':
                             if progress.n % config[phase].forward_times == 0:
                                 # update parameters every args.forward_times
-                                if progress.n != 0:
+                                if progress.n != 0 and loss_acc:
                                     loss_acc /= config[phase].forward_times
                                     optimizer.zero_grad()
                                     loss_acc.backward()
@@ -119,15 +121,10 @@ if __name__ == '__main__':
                             else:
                                 loss_acc += loss
 
-                        # if progress.n % config[phase].log_per_iter == 0:
-                        n_relations = 0
-                        for j in range(len(Ra)):
-                            n_relations += Ra[j].size(0)
+                        # n_relations = 0
+                        # for j in range(len(Ra)):
+                        #     n_relations += Ra[j].size(0)
                         progress.set_postfix(loss='%.3f' % np.sqrt(loss.item()), agg='%.3f' % (losses / (progress.n + 1)))
-                                             # , n_rel=n_relations)
-                            # print('%s [%d/%d][%d/%d] n_relations: %d, Loss: %.6f, Agg: %.6f' %
-                            #       (phase, epoch, args.n_epoch, i, len(dataloaders[phase]),
-                            #        n_relations, , ))
 
                         if progress.n % 100 == 0:
                             writer.add_scalar('{phase}/loss'.format(phase=phase),
@@ -140,6 +137,11 @@ if __name__ == '__main__':
                         if phase == 'train' and progress.n > 0 and progress.n % config['train'].ckp_per_iter == 0:
                             torch.save(model.state_dict(), '%s/ckpt/net_epoch_%d_iter_%d.pth' % (config['train'].outf, epoch, progress.n))
 
+                        progress.update()
+
+                if phase == 'train':
+                    torch.save(model.state_dict(), '%s/ckpt/net_epoch_%d_iter_%d.pth' % (config['train'].outf, epoch + 1, 0))
+
                 losses /= len(loader[phase])
                 progress.set_postfix(loss=losses, best=best_valid_loss)
                 writer.add_scalar('{phase}/mean_loss'.format(phase=phase),
@@ -149,10 +151,7 @@ if __name__ == '__main__':
                                    best_valid_loss,
                                   (epoch + 1) * len(dataset[phase]))
 
-                # print('%s [%d/%d] Loss: %.4f, Best valid: %.4f' %
-                #       (phase, epoch, args.n_epoch, losses, best_valid_loss))
-
-                if phase == 'valid':
+                if phase == 'eval':
                     scheduler.step(losses)
                     if losses < best_valid_loss:
                         best_valid_loss = losses
